5e575c48f33462d63f4f260626519d0581570af2
antoine broume <antoine.broume@hotmail.fr>
2021-03-09 14:28:52 +0100

    Van model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_220615) do
+ActiveRecord::Schema.define(version: 2021_03_09_114937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,18 @@ ActiveRecord::Schema.define(version: 2021_03_07_220615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vans", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.integer "passengers"
+    t.text "description"
+    t.integer "price_per_night"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_vans_on_user_id"
+  end
+
+  add_foreign_key "vans", "users"
 end
