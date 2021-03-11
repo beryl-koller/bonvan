5b3dc72c3399db892311dcc34aace4892e4a052a
SophieCrenet <sophie.crenet@gmail.com>
2021-03-11 14:44:55 +0100

    van show with datas ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
-  
+
   has_many :vans
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :first_name, :last_name, presence: true
   validates :email, uniqueness: true
+
+  def full_name
+    first_name + " " + last_name
+  end
+
 end
